6e0001f5224cb4e6a8b61b48d32c7660bf382c2b
TavkirS <taukirofficial15@gmail.com>
2025-06-01 22:54:53 +0530

    Fix: Add data migration for initial categories and update build process

--- build.sh
+++ build.sh
@@ -2,13 +2,17 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
 
 # Create database directory if it doesn't exist
 mkdir -p data
 
-# Run migrations
-python manage.py makemigrations
+# Run migrations in the correct order
+python manage.py makemigrations tools
+python manage.py makemigrations quiz
+python manage.py migrate tools --noinput
+python manage.py migrate quiz --noinput
 python manage.py migrate --noinput
 
 # Collect static files
